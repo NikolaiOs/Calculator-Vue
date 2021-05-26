977a61e8c65d1c9a8005a3cf0115069b2b15e5e5
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2021-05-26 13:33:16 +0300

    Calculator-Vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <Calculator msg="Calculator"/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.logo {
+  width: 200px;
+  align-self: center;
+}
+</style>
